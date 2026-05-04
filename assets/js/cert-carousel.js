816691165b21b6e2fa8349e53f196ef6ff37ed96
jennymae08 <jennymaedelatorre15@gmail.com>
2026-05-04 09:33:38 +0800

    remove bg for mobile, remove hamburger border, nya ambot naunsa nang carousel

--- assets/js/cert-carousel.js
+++ assets/js/cert-carousel.js
@@ -1,3 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const myCarouselElement = document.querySelector('#certCarousel');
+    
+    if (!myCarouselElement) return; 
+
+    const isDesktop = window.innerWidth >= 768;
+
+    if (isDesktop) {
+        // WEB/DESKTOP
+        const desktopCarousel = new bootstrap.Carousel(myCarouselElement, {
+            interval: 3000,
+            pause: 'hover'
+        });
+        desktopCarousel.cycle(); 
+    } else {
+        // MOBILE
+        new bootstrap.Carousel(myCarouselElement, {
+            interval: false, 
+            ride: false     
+        });
+    }
+});
+
+
 function viewCert(imgSrc, title) {
     const modalEl = document.getElementById('certModal');
     const modalImg = document.getElementById('modalImg');
@@ -6,10 +30,7 @@ function viewCert(imgSrc, title) {
     if (modalImg && certTitle && modalEl) {
         modalImg.src = imgSrc;
         certTitle.innerText = title;
-
-        
         const modal = bootstrap.Modal.getOrCreateInstance(modalEl);
-
         modal.show();
     }
 }
